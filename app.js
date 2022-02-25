8f9934dbc10d26be1f497c38be70f2bda741ce32
Apoorav07 <apooravsinghal666@gmail.com>
2022-02-25 23:22:31 +0530

    Dom api  selection methods

---
+++ app.js
@@ -0,0 +1,9 @@
+var heading =document.getElementById('heading')
+var para=document.getElementsByClassName('para')
+var tags=document.getElementsByTagName('a')
+var query=document.querySelector('.para')
+
+
+console.log(query)
+
+
