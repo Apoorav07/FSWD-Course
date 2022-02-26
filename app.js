4c9fc7289fabc50bd1a50d23f9c63fed189ef13e
Apoorav07 <apooravsinghal666@gmail.com>
2022-02-27 04:53:31 +0530

    adding items in a list with js

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var input= document.querySelector('input')
 var list=document.querySelector('ul')
 
 const callbackfunc = (event) => {
-  console.log(event.target)
-  console.log(input.value)
+  const inputvalue = input.value
+  const element = document.createElement('li')
+  const textnode = document.createTextNode(inputvalue)
+  element.appendChild(textnode)
+  list.appendChild(element)
+
 }
 
 button.addEventListener('click',callbackfunc)
