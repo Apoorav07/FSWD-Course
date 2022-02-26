229ecfc1c9c8a6461ebb5874933bdb080250a135
Apoorav07 <apooravsinghal666@gmail.com>
2022-02-27 05:29:23 +0530

    switching between dark mode and light mode with js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var button=document.querySelector('button')
-var input= document.querySelector('input')
-var list=document.querySelector('ul')
+var body= document.querySelector('body')
+
 
 const callbackfunc = (event) => {
-  const inputvalue = input.value
-  const element = document.createElement('li')
-  const textnode = document.createTextNode(inputvalue)
-  element.appendChild(textnode)
-  list.appendChild(element)
-
+  body.classList.toggle('dark')
+  
 }
+
+
 
 button.addEventListener('click',callbackfunc)
 
