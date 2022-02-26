7c017f5b6c151a70933b5b68b4596dbbbc8a9ae0
Apoorav07 <apooravsinghal666@gmail.com>
2022-02-27 02:54:13 +0530

    function expressions

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-function greet(Name='john',age='99') {
-  console.log(Name,age)
-  // console.log('hello')
+// var greet=function (Name='john',age='99') {
+  
+//   greet(Name,age)
+//   // console.log('hello')
+// }
+// console.log('apoo',19)
+
+// // here john and 99 are set as default value
+
+// console.log(add(10,20))
+
+// function add(a=0,b=0){
+  
+//   var sum =a+b
+//   return sum
+// }
+
+
+var add=function (a=0,b=0){
+  
+  var sum =a+b
+  return sum
 }
-
-greet()
-
-
+console.log(add(10,20))
