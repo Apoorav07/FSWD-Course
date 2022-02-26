cacfd8739b3569b4185538e234759678c8b9bf88
Apoorav07 <apooravsinghal666@gmail.com>
2022-02-26 23:16:07 +0530

    adding dark mode with javascript

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var heading =document.getElementById('heading')
-
+var body=document.querySelector('body')
 console.log(heading)
 heading.innerHTML='i was added by JS'
 heading.style='color:red;font-size:3rem'
 
 heading.classList.add('five')
 heading.classList.remove('one')
-console.log(heading.classList)
+body.classList.add('dark')
