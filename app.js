bdba30977fe2272f805b90cd59d5b60595c403c3
Apoorav07 <apooravsinghal666@gmail.com>
2022-03-20 02:08:58 +0530

    calculater using conditionals and js

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-const a = document.querySelectorAll('input') [0]
-const b = document.querySelectorAll('input') [1]
-const add=document.querySelector('#add')
-const multiply=document.querySelector('#multiply')
-const resultbox=document.querySelector('.result')
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+const add = document.querySelector('#add')
+const multiply = document.querySelector('#multiply')
+const resultbox =document.querySelector('.result')
+
+// const sum=()=>{
+//   const result = parseInt(a.value) + parseInt(b.value)
+//   resultbox.innerHTML=result
+
+// }
 
 
-const sum= () => {
-  const result=parseInt(a.value)+parseInt(b.value)
-  resultbox.innerHTML=result
+// const into=()=>{
+//   const result = parseInt(a.value) * parseInt(b.value)
+//   resultbox.innerHTML=result
+
+// }
+const calculate=(event,operation)=>{
+  // if (operation =='add'){
+  //   console.log('adding')
+  // }else if(operation =='multiply'){
+  //   console.log('multiply')
+  // }
+    switch (operation) {
+      case "add":
+        resultbox.innerHTML=parseInt(a.value) + parseInt(b.value)
+        break
+      case "multiply":
+        resultbox.innerHTML=parseInt(a.value) * parseInt(b.value)
+        break
+      default:
+        resultbox.innerHTML="not a valid operation"
+    }
+
+
+
+
 }
-add.addEventListener('click',sum)
 
 
-const into= () => {
-  const result=parseInt(a.value)*parseInt(b.value)
-  resultbox.innerHTML=result
-}
-multiply.addEventListener('click',into)
+add.addEventListener('click',(event)=>{ calculate(event,'add')})
+multiply.addEventListener('click',(event)=>{ calculate(event,'multiply')})
