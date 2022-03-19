81187fc3fdb86ad64a7c59999f3b09ee12d41ce6
Apoorav07 <apooravsinghal666@gmail.com>
2022-03-20 03:41:45 +0530

    array methods

--- app.js
+++ app.js
@@ -15,9 +15,13 @@
 
 
 
-var arr=[1,2,3,4,5,6,7,8]
-var new_arr =arr.filter((element,index)=>{
-  if(element<4)return true
-})
+// var arr=[1,2,3,4,5,6,7,8]
+// var new_arr =arr.filter((element,index)=>{
+//   if(element<4)return true
+// })
 
-console.log(new_arr)
+// console.log(new_arr)
+
+var arr=[1,2,3,4,5,6,7,8]
+arr.splice(3,3)
+console.log(arr)
