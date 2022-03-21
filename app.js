2d31aae5c74578890ed2f232b184c609d2c6ae7b
Apoorav07 <apooravsinghal666@gmail.com>
2022-03-22 02:13:04 +0530

    creating an object in js

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-var button = document.querySelector('button')
-var input=document.querySelector('input')
-var list=document.querySelector('ul')
+let car={}
 
-var chores=[]
-
-var deleteItem=(value)=>{
-  const index = chores.indexOf(value)
-  chores.splice(index,1)
-  console.log(chores)
-}
-
-const callbackfunc=(event)=>{
-
-const inputvalue=input.value
-if(chores.includes(inputvalue)) {
-  console.log('already exists')
-}
-else{
-  chores.push(inputvalue)
-  const element=document.createElement('li')
-  const textnode=document.createTextNode(inputvalue)
-  element.appendChild(textnode)
-  list.appendChild(element)
-  element.addEventListener('click',(e)=>{e.target.remove()})
-}
-}
-button.addEventListener('click',callbackfunc)
+console.log(car)
+console.log(typeof car)
