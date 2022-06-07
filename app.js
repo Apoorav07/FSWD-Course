77a24d83b8dde1dfed03a84757707a43e1d662d9
Apoorav07 <apooravsinghal666@gmail.com>
2022-06-08 04:19:32 +0530

    fetch api and then method

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-let value = new Promise((resolve,reject) => {
-  setTimeout(() => {
-    reject('Hello')
-  }, 3000)
-})
+let response = fetch('http://jsonplaceholder.typicode.com/users').then(res => {
+  let parseddata =res.json()
+  console.log(parseddata)
+  return parseddata
+}).then(data => {console.log(data)})
 
-setTimeout(() => {
-  console.log(value)
-}, 4000)
