17448ed88710d4c373ec2682431becc480a57232
Apoorav07 <apooravsinghal666@gmail.com>
2022-06-08 23:39:08 +0530

    deleting and element from list with window prompt method

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-let response = fetch('http://jsonplaceholder.typicode.com/users').then(res => {
-  let parseddata =res.json()
-  console.log(parseddata)
-  return parseddata
-}).then(data => {console.log(data)})
+// let response = fetch('http://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => {console.log(data)})
+
+const list =document.querySelector('ul')
+
+
+var arr = ['one','two','three','four']
+
+arr.forEach(chore =>{
+ const element =document.createElement('li')
+ const textnode =document.createTextNode(chore)
+ element.appendChild(textnode)
+ console.log(element)
+ 
+ element.addEventListener('click', (e) => {
+   
+   const userinput =window.prompt(`are you sure you want to delete "${chore}" `)
+   if (userinput === 'YES') e.target.remove('chore')
+ })
+
+ list.appendChild(element)
+})
+
 
