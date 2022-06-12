dabc7d9c9f29b1ae0e6dc08f85387fb9720b2e27
Apoorav07 <apooravsinghal666@gmail.com>
2022-06-13 00:47:39 +0530

    accessing exports in a module

---
+++ index.js
@@ -0,0 +1,10 @@
+// module : module is a incapsulated block of code and module is just an object inside javascript
+
+const greet = () => 'hi'
+module.exports.greet = greet
+console.log(module)
+
+
+const geet = () => 'hi'
+module.exports.something = 'hi'
+console.log(module)
