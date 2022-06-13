09327876c8938adb84500a4648e452ee12d41def
Apoorav07 <apooravsinghal666@gmail.com>
2022-06-14 02:43:38 +0530

    creating a server with http module and doing routing in a server

---
+++ server.js
@@ -0,0 +1,54 @@
+// const http =require('http')
+// const fs = require('fs')
+// const path = require('path')
+
+
+// const server =http.createServer((request,response)=>{
+//   // console.log(request)
+//   // console.log(request.url)
+//   // console.log(request.headers)
+//   // console.log(request.method)
+//   // const path = request.url
+//    const markup = fs.readFileSync(path.resolve('./index.html'))
+//   response.write(markup)
+//   response.end()
+// })
+
+// console.log(server)
+
+// server.listen(3000,()=>{
+//   console.log(`server listening at PORT: ${3000}`)
+// })
+
+
+
+
+
+const http =require('http')
+const fs = require('fs')
+const path = require('path')
+
+
+const server =http.createServer((request,response)=>{
+  // console.log(request)
+  // console.log(request.url)
+  // console.log(request.headers)
+  // console.log(request.method)
+  // const path = request.url
+   const {url }=request
+   if(url == '/login'){
+    response.write('<h1>LOGIN</h1>')
+    response.end()
+   }
+   if (url=='/signup'){
+    response.write('<h1>SIGNUP</h1>')
+    response.end()
+   }
+})
+
+
+
+server.listen(3000,()=>{
+  console.log(`server listening at PORT: ${3000}`)
+})
+
