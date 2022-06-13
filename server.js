78291d740f07822cfd1a4d15c452eece4ec749fa
Apoorav07 <apooravsinghal666@gmail.com>
2022-06-14 03:20:25 +0530

    creating a server with express

--- server.js
+++ server.js
@@ -20,35 +20,64 @@
 //   console.log(`server listening at PORT: ${3000}`)
 // })
 
+// above we created a server
+
+
+
+// const http =require('http')
+// const fs = require('fs')
+// const path = require('path')
+
+
+// const server =http.createServer((request,response)=>{
+//   // console.log(request)
+//   // console.log(request.url)
+//   // console.log(request.headers)
+//   // console.log(request.method)
+//   // const path = request.url
+//    const {url }=request
+//    if(url == '/login'){
+//     response.write('<h1>LOGIN</h1>')
+//     response.end()
+//    }
+//    if (url=='/signup'){
+//     response.write('<h1>SIGNUP</h1>')
+//     response.end()
+//    }
+// })
+
+
+
+// server.listen(3000,()=>{
+//   console.log(`server listening at PORT: ${3000}`)
+// })
+
+// above thing is called routing
 
 
 
 
-const http =require('http')
-const fs = require('fs')
-const path = require('path')
 
 
-const server =http.createServer((request,response)=>{
-  // console.log(request)
-  // console.log(request.url)
-  // console.log(request.headers)
-  // console.log(request.method)
-  // const path = request.url
-   const {url }=request
-   if(url == '/login'){
-    response.write('<h1>LOGIN</h1>')
-    response.end()
-   }
-   if (url=='/signup'){
-    response.write('<h1>SIGNUP</h1>')
-    response.end()
-   }
+// NOW WE WILL CREATE A SERVER WITH EXPRESS
+
+
+const express = require('express')
+const app =express()
+
+app.get('/login',(req,res) =>{
+  
+  res.send('hemmlo login pamji')
+})
+
+app.get('/logout',(req,res) =>{
+  
+  res.send('hemmlo logout pamji')
 })
 
 
-
-server.listen(3000,()=>{
-  console.log(`server listening at PORT: ${3000}`)
+app.listen(3000,()=>{
+  console.log('server litening to port 3000')
 })
+
 
