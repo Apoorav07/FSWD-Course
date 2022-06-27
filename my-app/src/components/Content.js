511629c93c7bbf7992786a58699baf6b2bcd8836
Apoorav07 <apooravsinghal666@gmail.com>
2022-06-27 18:01:20 +0530

    adding and deleteing item from todolisgt with react

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -6,24 +6,29 @@ const Content = () => {
   const [value,setvalue] = useState('')
 
   const handlesubmit=()=>{
-    setitems(prev => [prev,value])
-    
+    setitems(prev => [...prev,value])
+    setvalue('')
       
 }
   const handleinput =(e)=>{
     setvalue(e.target.value)
   }
   
+ const handledelete =(item)=>{ 
+    console.log(item)
+    setitems(prev => prev.filter(i => i !== item))
+ }
+
   return(
     <>
     
      <h2>Todo</h2>
      
-      <input  onChange={handleinput} type="text" ></input>
+      <input value={value} onChange={handleinput} type="text" ></input>
       <button onClick={handlesubmit} type="submit" >ADD</button>
      
      <ul>
-      {items.map( item => <li>{item}</li>)}
+      {items.map( item => <li>{item} <button onClick={()=>{handledelete(item)}}>Delete</button> </li>)}
       
      </ul>
      
