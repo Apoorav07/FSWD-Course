b8ad50425f7d4ba3e2fb2c301044166cccdc516d
Apoorav07 <apooravsinghal666@gmail.com>
2022-06-30 06:05:17 +0530

    routing in react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,19 @@
 import {useState} from 'react';
-import List from './components/List'
-
+import Details from './components/Detail';
+import Products from './components/Products';
+import {Routes,Route} from 'react-router-dom'
+import Navbar from './layout/Navbar'
 const App =()=> {
 
   
 
   return (
     <div>
-    <List/>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Products/>} />
+        <Route path='/product/:name' element={<Details/>} />
+      </Routes>
     </div>
     
     
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,17 +6,36 @@ import { createStore } from 'redux';
 import reducer from './reducer/reducer';
 import { Provider } from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
+import {BrowserRouter} from 'react-router-dom'
 
-const chores =[{id:1,text:"take the medicine"},{id:2,text:"eat food"},{id:3,text:"sleep"}]
+const products =
+[
+  {id:1,
+    name:"iphone 11",
+    description:"this ia iphone 11"
+  },
+
+{id:2,
+  name:"iphone 12",
+  description:"this is iphone 12"
+},
+
+{id:3,
+  name:"iphone 13",
+  description:"this is iphone 13"
+}
+]
 
 
-const store = createStore(reducer,chores,composeWithDevTools())
+const store = createStore(reducer,products,composeWithDevTools())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store} >
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
